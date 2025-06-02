bf6259b379c21c372b1648085ac24b5efb7c5d1d
Gunnar Ingi Stefánsson <gunnaringi3@gmail.com>
2025-06-02 12:15:50 +0200

    utils for layout measurements

---
+++ source/utilities/layout-measurements.tsx
@@ -0,0 +1,128 @@
+import type { DOMElement } from "ink";
+
+const FlexDirection = {
+  Column: 0,
+  ColumnReverse: 1,
+  Row: 2,
+  RowReverse: 3
+} as const;
+
+export function measureLeft(node: DOMElement) {
+  let left = node.yogaNode?.getComputedLeft() ?? 0;
+  let currentNode = node;
+  let parent = node?.parentNode;
+
+  while (parent) {
+    const direction = parent?.yogaNode?.getFlexDirection();
+    left += parent.yogaNode?.getComputedLeft() ?? 0;
+
+    if (direction === FlexDirection.Row) {
+      for (const child of parent.childNodes) {
+        if (child === currentNode) {
+          break;
+        } else {
+          const width = child.yogaNode?.getComputedWidth();
+          left += width ?? 0;
+        }
+      }
+    } else if (direction === FlexDirection.RowReverse) {
+      const children = Array.from(parent.childNodes);
+      const currentIndex = children.indexOf(currentNode);
+      for (let i = currentIndex + 1; i < children.length; i++) {
+        const child = children[i];
+        if (child) {
+          const width = child.yogaNode?.getComputedWidth();
+          left += width ?? 0;
+        }
+      }
+    }
+
+    currentNode = parent;
+    parent = parent.parentNode;
+  }
+
+  return left;
+}
+
+export function measureTop(node: DOMElement) {
+  let top = node.yogaNode?.getComputedTop() ?? 0;
+  let currentNode = node;
+  let parent = node?.parentNode;
+
+  while (parent) {
+    const direction = parent?.yogaNode?.getFlexDirection();
+    top += parent.yogaNode?.getComputedTop() ?? 0;
+
+    if (direction === FlexDirection.Column) {
+      for (const child of parent.childNodes) {
+        if (child === currentNode) {
+          break;
+        } else {
+          const height = child.yogaNode?.getComputedHeight();
+          top += height ?? 0;
+        }
+      }
+    } else if (direction === FlexDirection.ColumnReverse) {
+      const children = Array.from(parent.childNodes);
+      const currentIndex = children.indexOf(currentNode);
+      for (let i = currentIndex + 1; i < children.length; i++) {
+        const child = children[i];
+        if (child) {
+          const height = child.yogaNode?.getComputedHeight();
+          top += height ?? 0;
+        }
+      }
+    }
+
+    currentNode = parent;
+    parent = parent.parentNode;
+  }
+
+  return top;
+}
+
+export function getBoundingClientRect(node: DOMElement) {
+  let _left: number | undefined;
+  let _top: number | undefined;
+  let _width: number | undefined;
+  let _height: number | undefined;
+
+  return {
+    get left() {
+      if (_left === undefined) {
+        _left = measureLeft(node);
+      }
+      return _left;
+    },
+    get top() {
+      if (_top === undefined) {
+        _top = measureTop(node);
+      }
+      return _top;
+    },
+    get width() {
+      if (_width === undefined) {
+        _width = node.yogaNode?.getComputedWidth() ?? 0;
+      }
+      return _width;
+    },
+    get height() {
+      if (_height === undefined) {
+        _height = node.yogaNode?.getComputedHeight() ?? 0;
+      }
+      return _height;
+    },
+    get right() {
+      return this.left + this.width;
+    },
+    get bottom() {
+      return this.top + this.height;
+    },
+    get x() {
+      return this.left;
+    },
+    get y() {
+      return this.top;
+    }
+  };
+}
